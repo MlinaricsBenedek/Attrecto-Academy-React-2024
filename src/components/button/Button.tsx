652bade48fe3bd72094b0d31bc59f93bbd77a51a
MlinaricsBenedek <144675299+MlinaricsBenedek@users.noreply.github.com>
2024-10-20 00:34:12 +0200

    hibas

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -2,6 +2,11 @@ import classNames from "classnames";
 import React from "react";
 import { ButtonHTMLAttributes } from "react";
 interface ButtonProps extends ButtonHTMLAttributes {
+=======
+import React, { ButtonHTMLAttributes } from "react";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>{
+>>>>>>> 7f769e92e6f75f59c66cc5c98bd9a854ecb27a54
   color?: "primary" | "secondary" | "danger";
   children: React.ReactNode;
   className?: string;
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,4 +1,7 @@
+<<<<<<< HEAD
 import { number, string } from "yup";
+=======
+>>>>>>> 7f769e92e6f75f59c66cc5c98bd9a854ecb27a54
 import { UserFormValues, UserModel } from "../models/user.model";
 import request, { Methods } from "../util/request";
 
@@ -7,6 +10,7 @@ class UserService {
     return request<UserModel[]>({ method: Methods.GET, resource: "users" });
   }
 
+<<<<<<< HEAD
 async getUser(id: string | number){
   return request<UserModel>({method:Methods.GET,resource:`users/${id}`});
 
@@ -14,6 +18,23 @@ async getUser(id: string | number){
 async updateUser(id:string|number,data:UserFormValues)
 {
   return request<UserModel>({method:Methods.PATCH, resource:`users${id}`,data});
+=======
+  async geUser(id: string | number){
+    return request<UserModel>({ method: Methods.GET, resource: `users/${id}` });
+  }
+
+  async updateUser(id: string | number, data: UserFormValues) {
+    return request<UserModel>({ method: Methods.PATCH, resource: `users/${id}`, data });
+  }
+
+  async createUser(data: UserFormValues) {
+    return request<UserModel>({ method: Methods.POST, resource: "users", data });
+  }
+
+  async deleteUser(id: string | number) {
+    return request<UserModel>({ method: Methods.DELETE, resource: `users/${id}` });
+  }
+>>>>>>> 7f769e92e6f75f59c66cc5c98bd9a854ecb27a54
 }
 
 async createUser(data:UserFormValues)
