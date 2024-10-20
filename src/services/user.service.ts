fbf1bc6eb8a9b2c53f9b4f94f78bd36bba2908c7
MlinaricsBenedek <144675299+MlinaricsBenedek@users.noreply.github.com>
2024-10-20 15:24:02 +0200

    userservice

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-import { number, string } from "yup";
-=======
->>>>>>> 7f769e92e6f75f59c66cc5c98bd9a854ecb27a54
 import { UserFormValues, UserModel } from "../models/user.model";
 import request, { Methods } from "../util/request";
 
@@ -10,7 +6,6 @@ class UserService {
     return request<UserModel[]>({ method: Methods.GET, resource: "users" });
   }
 
-<<<<<<< HEAD
 async getUser(id: string | number){
   return request<UserModel>({method:Methods.GET,resource:`users/${id}`});
 
@@ -18,23 +13,6 @@ async getUser(id: string | number){
 async updateUser(id:string|number,data:UserFormValues)
 {
   return request<UserModel>({method:Methods.PATCH, resource:`users${id}`,data});
-=======
-  async geUser(id: string | number){
-    return request<UserModel>({ method: Methods.GET, resource: `users/${id}` });
-  }
-
-  async updateUser(id: string | number, data: UserFormValues) {
-    return request<UserModel>({ method: Methods.PATCH, resource: `users/${id}`, data });
-  }
-
-  async createUser(data: UserFormValues) {
-    return request<UserModel>({ method: Methods.POST, resource: "users", data });
-  }
-
-  async deleteUser(id: string | number) {
-    return request<UserModel>({ method: Methods.DELETE, resource: `users/${id}` });
-  }
->>>>>>> 7f769e92e6f75f59c66cc5c98bd9a854ecb27a54
 }
 
 async createUser(data:UserFormValues)
@@ -43,7 +21,7 @@ return request<UserModel>({method:Methods.POST, resource:"user",data})
 }
 async deleteUser(id:string|number)
 {
-  return request<UserModel>({method:Methods.DELETE, resource="user"})
+  return request<UserModel>({method:Methods.DELETE, resource: "user"})
 }
 }
 export const userService = new UserService();
